38fa791e4953b2713ca5f27c96cc4b95f2ad13ae
Steve Ham <steve.ham@kakaomobility.com>
2024-11-06 03:46:27 +0900

    initial commit.

--- AppleLocationCrash/ViewController.swift
+++ AppleLocationCrash/ViewController.swift
@@ -6,14 +6,80 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    private var backgroundActivitySession: CLBackgroundActivitySession?
+    private var needsLocationUpdate = false {
+        didSet {
+            updateStateLabel()
+        }
+    }
+    private let stateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Background Location Updates Disabled"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Create the buttons
+        let enableButton = UIButton(type: .system)
+        enableButton.setTitle("enableBackgroundLocation", for: .normal)
+        enableButton.addTarget(self, action: #selector(enableBackgroundLocation), for: .touchUpInside)
+        
+        let disableButton = UIButton(type: .system)
+        disableButton.setTitle("disableBackgroundLocation", for: .normal)
+        disableButton.addTarget(self, action: #selector(disableBackgroundLocation), for: .touchUpInside)
+        
+        // Create a stack view to hold the label and buttons
+        let stackView = UIStackView(arrangedSubviews: [stateLabel, enableButton, disableButton])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add the stack view to the main view
+        view.addSubview(stackView)
+        
+        // Center the stack view in the view
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+//        NotificationCenter.default.addObserver(forName: UIApplication.didEnterBackgroundNotification, object: nil, queue: nil) { [weak self] _ in
+//            guard let self = self else { return }
+//            print("hsteve didEnterBackgroundNotification")
+//            if self.needsLocationUpdate {
+//                self.backgroundActivitySession = CLBackgroundActivitySession() // === self.backgroundActivitySession = nil
+//            } else {
+//                self.backgroundActivitySession = nil
+//            }
+//        }
     }
-
-
+    
+    // Update label text based on the state of background location updates
+    private func updateStateLabel() {
+        stateLabel.text = needsLocationUpdate ? "Background Location Updates Enabled" : "Background Location Updates Disabled"
+    }
+    
+    // Actions for the buttons
+    @objc func enableBackgroundLocation() {
+        print("hsteve enableBackgroundLocation")
+        needsLocationUpdate = true
+        backgroundActivitySession = CLBackgroundActivitySession()
+//        let session = CLBackgroundActivitySession()
+//        print("hsteve session \(session)")
+    }
+    
+    @objc func disableBackgroundLocation() {
+        print("hsteve disableBackgroundLocation")
+        needsLocationUpdate = false
+        backgroundActivitySession = nil
+    }
 }
-
